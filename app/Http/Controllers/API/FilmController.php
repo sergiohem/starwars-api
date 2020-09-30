5f9a19d9ac34608609430f3c4558f3aed90c3523
sergiohem <sergiohenriquemarques@outlook.com>
2020-09-29 21:46:37 -0300

    ajustes finais

--- app/Http/Controllers/API/FilmController.php
+++ app/Http/Controllers/API/FilmController.php
@@ -17,7 +17,7 @@ class FilmController extends Controller
     public function films() {
         $response = $this->apiSwapi->get();
 		$result = [];
-		
+
         if ($response->successful()) {
 
 			$films = $response->json()['results'];
@@ -44,56 +44,5 @@ class FilmController extends Controller
         }
 
         return json_encode($result);
-    }
-
-    public function prettyPrint($json, $html = false)
-    {
-        $tabcount = 0;
-		$result = '';
-		$inquote = false;
-		$ignorenext = false;
-
-		if ($html) {
-		    $tab = "&nbsp;&nbsp;&nbsp;";
-		    $newline = "<br/>";
-		} else {
-		    $tab = "\t";
-		    $newline = "\n";
-		}
-
-		for($i = 0; $i < strlen($json); $i++) {
-		    $char = $json[$i];
-
-		    if ($ignorenext) {
-		        $result .= $char;
-		        $ignorenext = false;
-		    } else {
-		        switch($char) {
-		            case '{':
-		                $tabcount++;
-		                $result .= $char . $newline . str_repeat($tab, $tabcount);
-		                break;
-		            case '}':
-		                $tabcount--;
-		                $result = trim($result) . $newline . str_repeat($tab, $tabcount) . $char;
-		                break;
-		            case ',':
-		                $result .= $char . $newline . str_repeat($tab, $tabcount);
-		                break;
-		            case '"':
-		                $inquote = !$inquote;
-		                $result .= $char;
-		                break;
-		            case '\\':
-		                if ($inquote) $ignorenext = true;
-		                $result .= $char;
-		                break;
-		            default:
-		                $result .= $char;
-		        }
-		    }
-		}
-
-		return $result;
     }
 }
